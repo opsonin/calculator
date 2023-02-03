a59e15fd1ab47b7ca3ffe44983ac63c8f41054ea
felixqui <felixqui@jnspcorp.com>
2023-02-03 12:36:47 +0900

    Add main.py

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 # ch 4.2.1 main.py
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-class Calculator(QWidget):
+from PyQt5.QtWidgets import QApplication, QWidget   # 애플리케이션 핸들러와 빈 GUI 위젯
+class Calculator(QWidget):  # QWidget 클래스를 상속받아서 클래스를 정리
 
     def __init__(self):
-        super().__init__()
-        self.initUI()
+        super().__init__()  # 부모 클래스 QWidget을 초기화
+        self.initUI()       # 나머지 초기화는 initUI 함수에 정의
 
     def initUI(self):
-        self.setWindowTitle('Calculator')
-        self.resize(256,256)
-        self.show()
+        self.setWindowTitle('Calculator')   # 윈도우에 표시되는 타이틀
+        self.resize(256,256)    # 윈도우 사이즈
+        self.show()             # 윈도우 화면이 표시되도록 설정
     
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    view = Calculator()
-    sys.exit(app.exec_())
+if __name__ == '__main__':      # pyqt는 애플리케이션 당 1개의 QApplication이 필요함
+    app = QApplication(sys.argv)    # QApplication 인스턴스 생성
+    view = Calculator()             # Calculator 윈도우 인스턴스 생성
+    sys.exit(app.exec_())           # 애플리케이션이 이벤트 처리를 하도록 루프 구동
