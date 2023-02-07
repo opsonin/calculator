b501dfdf9d30385f268582cce93424c6d6ef2615
jnspcorp <felixqui@jnspcorp.com>
2023-02-07 18:34:00 +0900

    Modify ui.py to add le1, le2 and db widgets

--- ui.py
+++ ui.py
@@ -1,6 +1,7 @@
-# ch 5.2.1 ui.py
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout)   # QHBoxLayout 추가
+# ch 6.2.1 ui.py
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox, QPlainTextEdit, QHBoxLayout, QLineEdit, QComboBox)   # QLineEdit, QComboBox 추가
 from PyQt5.QtGui import QIcon   # icon을 추가하기 위한 라이브러리
+from PyQt5 import QtCore        # 모듈 추가
 
 class View(QWidget):  # QWidget 클래스를 상속받아서 클래스를 정리
 
@@ -12,6 +13,20 @@ class View(QWidget):  # QWidget 클래스를 상속받아서 클래스를 정리
         self.te1 = QPlainTextEdit()     # 텍스트 에디트 위젯 생성
         self.te1.setReadOnly(True)      # 텍스트 에디트 위젯을 읽기만 가능하도록 수정
 
+        self.le1 = QLineEdit('0', self)     # 라인 에디터1 추가
+        self.le1.setAlignment(QtCore.Qt.AlignRight)     # 라인 에디터1 문자열 배치 설정
+
+        self.le2 = QLineEdit('0', self)     # 라인 에디터2 추가
+        self.le2.setAlignment(QtCore.Qt.AlignRight)     # 라인 에디터2 문자열 배치 설정
+
+        self.cb = QComboBox(self)               # 콤보 박스 추가
+        self.cb.addItems(['+', '-', '*', '/'])  # 콤보 박스 항목 추가(연산자로 사용)
+
+        hbox_formular = QHBoxLayout()       # 새로 정의한 위젯을 QHBoxLayout에 배치
+        hbox_formular.addWidget(self.le1)
+        hbox_formular.addWidget(self.cb)
+        hbox_formular.addWidget(self.le2)
+
         self.btn1 = QPushButton('Message', self)    # 버튼추가
         self.btn2 = QPushButton('Clear', self)      # 버튼 2 추가
 
@@ -20,10 +35,11 @@ class View(QWidget):  # QWidget 클래스를 상속받아서 클래스를 정리
         hbox.addWidget(self.btn1)   # 버튼 1 배치
         hbox.addWidget(self.btn2)   # 버튼 2 배치
         
-        vbox = QVBoxLayout()        # 수직 레이아웃 위젯 생성
-        vbox.addWidget(self.te1)    # 수직 레이아웃에 텍스트 에디트 위젯 추가
-        vbox.addLayout(hbox)        # btn1 위치에 hbox를 배치
-        vbox.addStretch(1)          # 빈 공간
+        vbox = QVBoxLayout()            # 수직 레이아웃 위젯 생성
+        vbox.addWidget(self.te1)        # 수직 레이아웃에 텍스트 에디트 위젯 추가
+        vbox.addLayout(hbox_formular)   # hbox_formular 배치
+        vbox.addLayout(hbox)            # btn1 위치에 hbox를 배치
+        vbox.addStretch(1)              # 빈 공간
 
         self.setLayout(vbox)    # 빈 공간 - 버튼 - 빈 공간 순으로 수직 배치된 레이아웃 설정
 
