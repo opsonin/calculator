315961bae131b06d63705e6e0b5705cdfa3c57d1
jnspcorp <felixqui@jnspcorp.com>
2023-02-09 17:05:33 +0900

    Add mod function and modify calculate function in ctrl.py

--- ctrl.py
+++ ctrl.py
@@ -22,6 +22,8 @@ class Control:
                 return f'{num1} / {num2} = {self.div(num1, num2)}'
             elif operator == '^':
                 return f'{num1} ^ {num2} = {self.pow(num1, num2)}'
+            elif operator == '%':       # '%'를 입력했을 때 mod 연산 결과를 출력하도록 추가
+                return f'{num1} % {num2} = {self.mod(num1, num2)}'
             else:
                 return "Calculation Error"
         
@@ -59,4 +61,14 @@ class Control:
             return e
         
         return pow(a, b)
+
+    def mod(self, a, b):    # 나눗셈 연산의 나머지를 리턴하는 함수 추가
+        try:
+            if (b == 0):
+                raise Exception("divisor Error")
+        
+        except Exception as e:            
+            return e
+        
+        return a % b
         
